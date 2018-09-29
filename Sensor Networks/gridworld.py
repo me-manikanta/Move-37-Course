bcff318e330a654e133628e7c9ea4b5bcf104334
me-manikanta <manikantajreinstein97@gmail.com>
2018-09-29 18:57:40 +0530

    Added SensorNetworks
    -> Updated gridworld file

---
+++ Sensor Networks/gridworld.py
@@ -0,0 +1,191 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+
+class GridWorld:
+
+    _direction_deltas = [
+        (-1,0),
+        (1,0),
+        (0,1),
+        (0,-1)
+    ]
+
+    _num_actions = len(_direction_deltas)
+
+    def __init__(self, reward_grid, terminal_mask, obstacle_mask, action_probabilities, no_action_probabilities):
+        self.reward_grid = reward_grid
+        self.terminal_mask = terminal_mask
+        self.obstacle_mask = obstacle_mask
+
+        self._T = self._create_transition_matrix(
+            action_probabilities,
+            no_action_probabilities,
+            obstacle_mask
+        )
+
+    @property
+    def shape(self):
+        return self.reward_grid.shape
+
+    @property
+    def size(self):
+        return self.reward_grid.size
+
+    def run_value_iterations(self, discount = 1.0, iterations = 10):
+        utility_grids, policy_grids = self._init_utility_policy_storage(iterations)
+
+        utility_grid = np.zeros_like(self.reward_grid)
+        for i in range(iterations):
+            utility_grid = self.value_iteration(utility_grid=utility_grid)
+            policy_grids[:,:,i] = self.best_policy(utility_grid)
+            utility_grids[:,:,i] = utility_grid
+        
+        return policy_grids, utility_grids
+
+    def run_policy_iterations(self, discount = 1.0, iterations = 10):
+        utility_grids, policy_grids = self._init_utility_policy_storage(iterations)
+
+        policy_grid = np.random.randint(0, self._num_actions, self.shape)
+
+        utility_grid = self.reward_grid.copy()
+        for i in range(iterations):
+            policy_grid, utility_grid = self._policy_iteration(policy_grid=policy_grid,utility_grid=utility_grid)
+            policy_grids[:,:,i]=policy_grid
+            utility_grids[:,:,i]=utility_grid
+        
+        return policy_grids,utility_grids
+
+    def generate_experience(self, current_state_idx, action_index):
+        sr, sc = self.grid_indices_to_coordinates(current_state_idx)
+        next_state_probs = self._T[sr, sc, action_index, :, :].flatten()
+        next_state_idx = np.random.choice(np.arange(next_state_probs.size),p=next_state_probs)
+
+        return (next_state_idx, self.reward_grid.flatten()[next_state_idx], self.terminal_mask.flatten()[next_state_idx])
+    
+    def grid_indices_to_coordinates(self, indices=None):
+        if indices is None:
+            indices = np.arange(self.size)
+        return np.unravel_index(indices, self.shape)
+
+    def grid_coordinates_to_indices(self, coordinates = None):
+        if coordinates is None:
+            return np.arange(self.size)
+        return np.ravel_multi_index(coordinates, self.shape)
+
+    def best_policy(self, utility_grid):
+        M, N = self.shape
+        return np.argmax((utility_grid.reshape((1, 1, 1, M, N)) * self._T)
+                         .sum(axis=-1).sum(axis=-1), axis=2)
+
+    def _init_utility_policy_storage(self, depth):
+        M, N = self.shape
+        utility_grids = np.zeros((M, N, depth))
+        policy_grids = np.zeros_like(utility_grids)
+        return utility_grids, policy_grids
+
+    def _create_transition_matrix(self,
+                                  action_probabilities,
+                                  no_action_probability,
+                                  obstacle_mask):
+        M, N = self.shape
+
+        T = np.zeros((M, N, self._num_actions, M, N))
+
+        r0, c0 = self.grid_indices_to_coordinates()
+
+        T[r0, c0, :, r0, c0] += no_action_probability
+
+        for action in range(self._num_actions):
+            for offset, P in action_probabilities:
+                direction = (action + offset) % self._num_actions
+
+                dr, dc = self._direction_deltas[direction]
+                r1 = np.clip(r0 + dr, 0, M - 1)
+                c1 = np.clip(c0 + dc, 0, N - 1)
+
+                temp_mask = obstacle_mask[r1, c1].flatten()
+                r1[temp_mask] = r0[temp_mask]
+                c1[temp_mask] = c0[temp_mask]
+
+                T[r0, c0, action, r1, c1] += P
+
+        terminal_locs = np.where(self.terminal_mask.flatten())[0]
+        T[r0[terminal_locs], c0[terminal_locs], :, :, :] = 0
+        return T
+
+    def _value_iteration(self, utility_grid, discount=1.0):
+        out = np.zeros_like(utility_grid)
+        M, N = self.shape
+        for i in range(M):
+            for j in range(N):
+                out[i, j] = self._calculate_utility((i, j), discount, utility_grid)
+        return out
+
+    def _policy_iteration(self, *, utility_grid,
+                          policy_grid, discount=1.0):
+        r, c = self.grid_indices_to_coordinates()
+
+        M, N = self.shape
+
+        utility_grid = (
+            self.reward_grid +
+            discount * ((utility_grid.reshape((1, 1, 1, M, N)) * self._T)
+                        .sum(axis=-1).sum(axis=-1))[r, c, policy_grid.flatten()]
+            .reshape(self.shape)
+        )
+
+        utility_grid[self.terminal_mask] = self.reward_grid[self.terminal_mask]
+
+        return self.best_policy(utility_grid), utility_grid
+
+    def _calculate_utility(self, loc, discount, utility_grid):
+        if self.terminal_mask[loc]:
+            return self.reward_grid[loc]
+        row, col = loc
+        return np.max(
+            discount * np.sum(
+                np.sum(self._T[row, col, :, :, :] * utility_grid,
+                       axis=-1),
+                axis=-1)
+        ) + self.reward_grid[loc]
+
+    def plot_policy(self, utility_grid, policy_grid=None):
+        if policy_grid is None:
+            policy_grid = self.best_policy(utility_grid)
+        markers = "^>v<"
+        marker_size = 200
+        marker_edge_width = marker_size // 10
+        marker_fill_color = 'w'
+
+        no_action_mask = self.terminal_mask | self.obstacle_mask
+
+        utility_normalized = (utility_grid - utility_grid.min()) / \
+                             (utility_grid.max() - utility_grid.min())
+
+        utility_normalized = (255*utility_normalized).astype(np.uint8)
+
+        utility_rgb = cv2.applyColorMap(utility_normalized, cv2.COLORMAP_JET)
+        for i in range(3):
+            channel = utility_rgb[:, :, i]
+            channel[self.obstacle_mask] = 0
+
+        plt.imshow(utility_rgb[:, :, ::-1], interpolation='none')
+
+        for i, marker in enumerate(markers):
+            y, x = np.where((policy_grid == i) & np.logical_not(no_action_mask))
+            plt.plot(x, y, marker, ms=marker_size, mew=marker_edge_width,
+                     color=marker_fill_color)
+
+        y, x = np.where(self.terminal_mask)
+        plt.plot(x, y, 'o', ms=marker_size, mew=marker_edge_width,
+                 color=marker_fill_color)
+
+        tick_step_options = np.array([1, 2, 5, 10, 20, 50, 100])
+        tick_step = np.max(policy_grid.shape)/8
+        best_option = np.argmin(np.abs(np.log(tick_step) - np.log(tick_step_options)))
+        tick_step = tick_step_options[best_option]
+        plt.xticks(np.arange(0, policy_grid.shape[1] - 0.5, tick_step))
+        plt.yticks(np.arange(0, policy_grid.shape[0] - 0.5, tick_step))
+        plt.xlim([-0.5, policy_grid.shape[0]-0.5])
+        plt.xlim([-0.5, policy_grid.shape[1]-0.5])
